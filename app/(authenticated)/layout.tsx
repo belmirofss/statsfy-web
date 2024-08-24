f5f522ede80e2d41767afc0150bcb84419790dcb
belmirofss <belmirofss@gmail.com>
2024-08-24 16:50:04 -0300

    fix

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
   return (
     <div>
       <Header />
-      <main className="px-6 md:px-12 pb-6 pt-20 h-full flex flex-row justify-center w-full">
+      <main className="p-6 pt-20 h-full flex flex-row justify-center w-full">
         <div className="w-full md:max-w-lg">{children}</div>
       </main>
       <Footer />
