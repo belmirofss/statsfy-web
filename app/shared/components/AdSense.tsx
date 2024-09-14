5d694c57a49d6026eab84fc2602890861dead5be
belmirofss <belmirofss@gmail.com>
2024-09-14 17:50:06 -0300

    fix ads

--- app/shared/components/AdSense.tsx
+++ app/shared/components/AdSense.tsx
@@ -4,7 +4,7 @@ export const AdSense = () => {
   return (
     <Script
       async
-      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${process.env.PUBLISHER_ID}`}
+      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.PUBLISHER_ID}`}
       crossOrigin="anonymous"
       strategy="afterInteractive"
     />
