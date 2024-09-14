bb706b14c4e54d8576b9d36b6e2c2e041bbd532c
belmirofss <belmirofss@gmail.com>
2024-09-14 17:11:37 -0300

    fix

--- app/shared/constants.ts
+++ app/shared/constants.ts
@@ -3,3 +3,4 @@ export const BUY_ME_A_COFFEE_URL = "https://www.buymeacoffee.com/belmirofss";
 export const PLAYSTORE_URL = "https://play.google.com/store/apps/details?id=com.yabcompany.statsfy";
 export const PUBLISHER_ID = "pub-6575307967199593";
 
+
