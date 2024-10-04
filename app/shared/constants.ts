d7280fac8e69bfd076d334f7f04965de15b162aa
belmirofss <belmirofss@gmail.com>
2024-10-03 22:46:10 -0300

    improve web scrapping

--- app/shared/constants.ts
+++ app/shared/constants.ts
@@ -1,5 +1,5 @@
 export const SPOTIFY_API_ENDPOINT = "https://api.spotify.com";
-export const CHART_MASTERS_ENDPOINT = "https://chartmasters.org/wp-admin/admin-ajax.php";
+export const CHART_MASTERS_ENDPOINT = "https://chartmasters.org";
 export const BUY_ME_A_COFFEE_URL = "https://www.buymeacoffee.com/belmirofss";
 export const PLAYSTORE_URL = "https://play.google.com/store/apps/details?id=com.yabcompany.statsfy";
 
